fb583a0be9aae6048151533310cb1330fdd644a3
BOH DZOMO <bohdzomobobpeter@gmail.com>
2025-02-07 22:20:27 +0100

    removed unnecessary functions

--- set_complete.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "database.php";
-if (isset($_POST["id"])) {
-    $id = $_POST["id"];
-    $update_sql = "UPDATE `task` SET state = 'complete' WHERE id = ?";
-    $update_result = $handler->prepare($update_sql);
-    $update_result->execute(array($id));
-   
-}
